8650c647508c74651f42fbdd5e2c31b17fcc879b
Vicente <vicente@stardestroyer.inf.ufrgs.br>
2026-03-31 12:48:59 -0300

    implementado agregação considerando peso dos scores

--- tests/tta/tta_config.py
+++ tests/tta/tta_config.py
@@ -62,6 +62,7 @@ def run_tta(cfg, runner, angles):
         augmented_boxes = []
         all_scores = []
         print(f'Batch: {i}')
+        padding_ok = False
         for angle in angles:
             batch_copy = data_batch.copy()
             
@@ -78,28 +79,41 @@ def run_tta(cfg, runner, angles):
 
             else:
                 metainfo = batch_copy['data_samples'][0].metainfo
+               
                 img = batch_copy['inputs'][0].permute(1,2,0)
-                
-
-                r_image = mmcv.imrotate(img = img.cpu().numpy(), angle = angle)
+                if padding_ok != True:
+                    h,w = metainfo['img_shape'][0:2]
+                    diagonal = sqrt(h*h + w*w)
+                    diagonal = ceil(diagonal)
+                    pad_top = floor((diagonal-h)/2)
+                    pad_left = floor((diagonal-w)/2)
+                    padded_img = mmcv.image.impad(img = img.cpu().numpy(), padding=(pad_left, pad_top))
+                    shape = padded_img.shape
+                    padding_ok = True
+                    
+                r_image = mmcv.imrotate(img = padded_img, angle = angle)
                 new_img = r_image
+                
                 r_image= img.new_tensor(r_image).permute(2,0,1)
+                
                 batch_copy['inputs'][0] = r_image
                 outputs = runner.model.test_step(batch_copy)
                 
                 pred_sample = outputs[0]
-                # draw_rotated_boxes(new_img, pred_sample.pred_instances.bboxes, f'tests/tta/images/{angle}check.jpg')
+                
+                ori_img = cv2.imread(metainfo['img_path'])
+                
                 if len(pred_sample.pred_instances.bboxes) == 0:
                     continue
-
-                new_boxes = invert_rotation(pred_sample.pred_instances.bboxes, angle, metainfo['ori_shape'])
+                
+                new_boxes = invert_rotation(pred_sample.pred_instances.bboxes, angle, shape, metainfo['scale_factor'] ,padding=(pad_left, pad_top))
+                # draw_rotated_boxes(ori_img, new_boxes, f'tests/tta/images/{angle}check.jpg')
                 
                 augmented_boxes.append(new_boxes)
                 all_scores.append(pred_sample.pred_instances.scores)
-
+        
         if len(augmented_boxes) != 0:
                     
-        
             augmented_boxes = torch.cat(augmented_boxes)
             all_scores = torch.cat(all_scores)
             sorted_scores = torch.sort(all_scores, descending=True)
@@ -113,38 +127,52 @@ def run_tta(cfg, runner, angles):
             iou_matrix[iou_matrix>=iou_trh] = 1.0
             iou_matrix[iou_matrix<iou_trh] = 0.0
             
+            
+            
             solo_ind = iou_matrix.sum(dim=0)==0
             
             iou_matrix.fill_diagonal_(1.0)
             
             iou_matrix = iou_matrix[solo_ind]
             
-            valid_ind = iou_matrix.sum(dim=1)>=ceil(len(angles)/2)
+            valid_ind = iou_matrix.sum(dim=1)>=ceil(len(angles)*0.5)
+            
+            
             
             iou_matrix = iou_matrix[valid_ind]
        
+            iou_matrix *= sorted_scores.values
 
+            
+            
             if iou_matrix.size()[0] != 0 :
                 
             
                 final_boxes, final_scores= merge_gaussian_boxes(augmented_boxes,iou_matrix,sorted_scores)
                 
-                
-                
                 new_instances = InstanceData()
-                new_instances.bboxes = final_boxes  # Agora com tamanho 10
-                new_instances.scores = final_scores # Agora com tamanho 10
+                new_instances.bboxes = final_boxes  
+                new_instances.scores = final_scores 
                 new_instances.labels = torch.zeros(len(final_boxes), dtype=torch.long, device=final_boxes.device)
                 
 
                 pred_sample.pred_instances = new_instances
+
+                evaluator.process(
+                data_samples=[pred_sample],
+                data_batch=data_batch
+                )
+
+                # draw_rotated_boxes(ori_img, f'tests/tta/images/{i}_img_check.jpg', final_boxes)
+            else:
+                evaluator.process(
+                data_samples=[],
+                data_batch=data_batch
+                )
         
+                # draw_rotated_boxes(ori_img, f'tests/tta/images/{i}_img_check.jpg')
         
-        
-        evaluator.process(
-        data_samples=[pred_sample],
-        data_batch=data_batch
-        )
+       
         
     
 
@@ -157,72 +185,79 @@ def run_tta(cfg, runner, angles):
 
 
 
-def invert_rotation(bboxes, angle_deg, shape):
+def invert_rotation(bboxes, angle_deg, img_shape, ori_scale, padding):
 
     angle_rad = angle_deg * (np.pi / 180.0)
     
     
-    h_pad, w_pad = shape[:2]
-    cx, cy = floor(w_pad / 2), floor(h_pad / 2)
+    h_pad, w_pad = img_shape[:2]
+    cx, cy = w_pad / 2.0, h_pad / 2.0
     
    
-    x = bboxes[:, 0] - cx
-    y = bboxes[:, 1] - cy
+    x = bboxes[:, 0] * ori_scale[0] - cx
+    y = bboxes[:, 1] * ori_scale[1] - cy
     
 
     cos_a = np.cos(-angle_rad)
     sin_a = np.sin(-angle_rad)
     
-    new_x = (x * cos_a - y * sin_a) + cx
-    new_y = (x * sin_a + y * cos_a) + cy
-
-   
+    
+    new_x = (x * cos_a - y * sin_a) + cx - padding[0]
+    new_y = (x * sin_a + y * cos_a) + cy - padding[1]
+    
     new_angle = bboxes[:, 4] - angle_rad
+
+    new_x, new_y = new_x/ori_scale[0], new_y/ori_scale[1]
     
     return torch.stack([new_x, new_y, bboxes[:, 2], bboxes[:, 3], new_angle], dim=-1)
 
 
-def draw_rotated_boxes(img, bboxes, save_path):
+def draw_rotated_boxes(img,save_path ,bboxes= None ):
    
     img_canvas = img.copy()
-    for box in bboxes:
-        cx, cy, w, h, angle = box.tolist()
-        
-        rect = ((cx, cy), (w, h), angle * 180 / np.pi)
-        box_pts = cv2.boxPoints(rect)
-        box_pts = np.int0(box_pts)
-        
-        cv2.drawContours(img_canvas, [box_pts], 0, (0, 255, 0), 2)
+    if bboxes!=None:
+        for box in bboxes:
+            cx, cy, w, h, angle = box.tolist()
+            
+            rect = ((cx, cy), (w, h), angle * 180 / np.pi)
+            box_pts = cv2.boxPoints(rect)
+            box_pts = np.int0(box_pts)
+            
+            cv2.drawContours(img_canvas, [box_pts], 0, (0, 255, 0), 2)
         
     cv2.imwrite(save_path, img_canvas)
 
-def merge_gaussian_boxes(all_boxes, iou_matrix ,sorted_scores):
+def merge_gaussian_boxes(augmented_boxes, iou_matrix ,sorted_scores):
     
-    final_gaus = []
-    final_scores= []
-    final_boxes = []
+    
+    
+    g_params = gc.rbbox_to_gaussian(augmented_boxes, scalar=1.0)
+        
+    aggregated_boxes = g_params.unsqueeze(0) * iou_matrix.unsqueeze(-1)
+    
+    
+    aggregated_boxes = aggregated_boxes.sum(dim=1) / iou_matrix.sum(dim=1).unsqueeze(-1)
 
-    g_params = gc.rbbox_to_gaussian(all_boxes, scalar=1.0)
-    
-    aggregation_list = g_params.unsqueeze(0) * iou_matrix.unsqueeze(-1)
    
-    for i, list in enumerate(aggregation_list):
-        num_boxes = iou_matrix[i].sum(dim=0)
-        if (num_boxes.item()>1):
-            ind = list.sum(dim=1) > 1
-            final_scores.append(sorted_scores.values[ind].max())
-            final_gaus.append(list.sum(dim=0)/num_boxes)
+    # for i, list in enumerate(aggregated_boxes):
+    #     # num_boxes = mask[i].sum(dim=0)
+        
+    #     # if (num_boxes.item()>1):
+    #     #     ind = list.sum(dim=1) > 1
+    #     #     final_scores.append(sorted_scores.values[ind].max())
+    #     #     final_gaus.append(list.sum(dim=0))
 
-        else:
-            ind = list.sum(dim=1) > 1
-            final_scores.append(sorted_scores.values[ind].max())
-            final_gaus.append(list.sum(dim=0))   
+        
+    #     print(list)
+    #     exit()
+    #     # final_scores.append(sorted_scores.values[ind].max())
+    # final_gaus.append(list)   
     
-    for tensor in final_gaus:
-        final_boxes.append(gc.gaussian_to_rbbox(tensor, 1.0))
-    
-    return torch.stack(final_boxes), torch.stack(final_scores)
+    # for tensor in final_gaus:
+    final_boxes = gc.gaussian_to_rbbox(aggregated_boxes, 1.0)
 
+    
+    return final_boxes, iou_matrix.max(dim=1).values.unsqueeze(-1)
 
 
 def main():
@@ -242,7 +277,7 @@ def main():
     runner.model.eval()
 
         
-    angles_for_aug= [0,45,90,135]
+    angles_for_aug= [0,45,90]
 
     run_tta(cfg, runner, angles_for_aug)
     
